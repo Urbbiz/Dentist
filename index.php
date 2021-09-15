69b2671bdf300d2421901373ee3d847bb7e48bee
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-09-15 10:50:59 +0300

    switch function added

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 
-echo "Hello If you want register please follow instructions below","\n";
-echo "Please enter your national ID number","\n";
+echo "Hello If you want register for appointment, please enter: 1 ","\n";
+echo "If you are medical personnel, please enter: 2 ","\n";
 
-$nationalId = trim(fgets(STDIN,1024));
+$input = trim(fgets(STDIN,10));
 
-echo "your national ID number is $nationalId.";
+switch ($input)
+{
+    case 1;
+        echo "Please enter your national ID number","\n";
+        $nationalId = trim(fgets(STDIN,1024));
+        echo "your national ID number is $nationalId.";
+
+        break;
+    case  2;
+        echo "You reached medical personnel data";
+
+}
+
+
