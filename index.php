9b176abb719456c6d6633e22c9ae97d15fcc61d0
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-09-20 18:16:07 +0300

    bootstrap added

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
 
+use src\App\Application;
+
+require __DIR__.'/bootstrap.php';
+require_once (__DIR__.'/vendor/autoload.php');
+
+
 echo "Hello! Follow instructions below:","\n";
 echo "Enter: 1 --Register new appointment. ","\n";
 echo "Enter: 2 --Edit appointment.","\n";
 echo "Enter: 3 --Delete appointment.","\n";
 echo "Enter: 4 -- Only for medical personnel","\n";
+
+$runApp = new Application();
+$runApp->run();
 //
 //$input = trim(fgets(STDIN,10));
 //
