7abf1505f0b06be64156c4f3d93ed7f189c19914
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-09-21 13:45:48 +0300

    psr-4 implemented

--- src/App/Application.php
+++ src/App/Application.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace src\App;
+namespace Dentist\App;
 
 class Application implements ApplicationInterface
 {
--- src/App/ApplicationInterface.php
+++ src/App/ApplicationInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace src\App;
+namespace Dentist\App;
 
 interface ApplicationInterface
 {
