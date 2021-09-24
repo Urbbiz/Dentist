81820aad96bd72398ae3b7e951f8c9eaca0f88b8
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-09-24 17:42:42 +0300

    small refactoring

--- src/Database/DatabaseManager.php
+++ src/Database/DatabaseManager.php
@@ -8,10 +8,10 @@ Use PDO;
 class DatabaseManager extends Database
 {
 //PUT DATA TO DB
-public function addPatient($getNationalId, $getName, $getEmail, $getPhone, $getDateTime)
+public function addPatient($nationalId, $name, $email, $phone, $dateTime)
 {
     $statement = $this->connect()->prepare("INSERT INTO patient(nationalId,name,email,phone,datetime)VALUES  (?, ?, ?, ?, ?)");
-    $statement->execute([$getNationalId, $getName, $getEmail, $getPhone, $getDateTime]);
+    $statement->execute([$nationalId, $name, $email, $phone, $dateTime]);
 }
 
 }
